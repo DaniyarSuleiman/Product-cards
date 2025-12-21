059d50dfd788f0c737487644c0fd65af5c04b396
Daniyar <daniyar.bin.kamil@gmail.com>
2025-12-21 10:07:57 +0500

    I did homework 7

---
+++ homework-7.js
@@ -0,0 +1,64 @@
+// #2
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const filteredNumbers = numbers.filter(number => number >= 5);
+console.log(filteredNumbers);
+
+// #3
+
+const kitchenAppliances = ['spoon', 'mug', 'plate', 'fork', 'knife'];
+const checkAppliances = kitchenAppliances.includes('spoon');
+console.log(checkAppliances);
+
+// #4
+
+const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const reverseArray = numbersArray.toReversed();
+console.log(reverseArray);
+
+// #6
+
+import { userComments } from './comments.js';
+
+// #7
+
+const filteredByEmailCom = userComments.filter(userComm => userComm.email.includes('.com'));
+console.log(filteredByEmailCom);
+
+// #8
+
+const filteredById = userComments.map(userComm => ({
+  ...userComm,
+  postId: userComm.id <= 5 ? 2 : 1
+}));
+console.log(filteredById)
+
+// #9
+
+const NameIdArr = userComments.map(({ id, name }) => {
+  return { id, name };
+});
+
+// #10
+
+const checkValidity = userComments.map(userComm => ({
+  ...userComm,
+  isInvalid: userComm.body.length > 180 ? true : false
+}));
+console.log(checkValidity);
+
+// #11
+
+const getEmailsArr = userComments.reduce(function(accumulator, email) {
+  return email;
+}, 0);
+console.log(getEmailsArr);
+
+const getEmails = userComments.reduce((acc, comment) => 
+  [...acc, comment.email], []);
+console.log(getEmails);
+
+// #12
+
+const emailsStr = getEmailsArr.toString();
+console.log(emailsStr);
